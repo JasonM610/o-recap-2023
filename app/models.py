eceee7bf8bb99b95226f4c39819f138e07255f3b
JasonM610 <46823715+JasonM610@users.noreply.github.com>
2023-09-07 23:11:43 -0400

    add simple data model for users, beatmap scores, user top scores

---
+++ app/models.py
@@ -0,0 +1,74 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    """
+    data received from request to '/users/{user}/{mode}'
+    https://osu.ppy.sh/docs/index.html#user
+    """
+    user_key = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    user_name = db.Column(db.String(40), nullable=False)
+    country_code = db.Column(db.String(2))
+    avatar_url = db.Column(db.String(255))
+    num_badges = db.Column(db.Integer, default=0)
+    yearly_playcount = db.Column(db.Integer, default=0)
+    yearly_replays_watched = db.Column(db.Integer, default=0)
+
+
+class BeatmapScore(db.Model):
+    """
+    data received from request to '/beatmaps/{beatmap}/scores/users/{user}/all'
+    https://osu.ppy.sh/docs/index.html#beatmapuserscore
+    https://osu.ppy.sh/docs/index.html#score
+    """
+    score_key = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    beatmap_id = db.Column(db.Integer, unique=True, nullable=False)
+    score_id = db.Column(db.Integer, unique=True, nullable=False)
+    accuracy = db.Column(db.Float)
+    pp = db.Column(db.Float)
+    mods = db.Column(db.String(40))
+    score = db.Column(db.Integer)
+    leaderboard_rank = db.Column(db.Integer)
+    max_combo = db.Column(db.Integer)
+    count_300 = db.Column(db.Integer)
+    count_100 = db.Column(db.Integer)
+    count_50 = db.Column(db.Integer)
+    count_miss = db.Column(db.Integer)
+    passed = db.Column(db.Boolean)
+    letter_grade = db.Column(db.String(2))
+    created_at = db.Column(db.DateTime)
+    mode_int = db.Column(db.SmallInteger)
+
+
+class UserBestScore(db.Model):
+    """
+    data received from request to '/users/{user}/scores/best?limit=100&mode={mode}'
+    https://osu.ppy.sh/docs/index.html#get-user-scores
+    https://osu.ppy.sh/docs/index.html#score
+    """
+    score_key = db.Column(db.Integer, primary_key=True)    
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    beatmap_id = db.Column(db.Integer, unique=True, nulllable=False)
+    score_id = db.Column(db.Integer, unique=True, nullable=False)
+    accuracy = db.Column(db.Float)
+    pp = db.Column(db.Float)
+    mods = db.Column(db.String(40))
+    score = db.Column(db.Integer)
+    best_performance_rank = db.Column(db.Integer)
+    letter_grade = db.Column(db.String(2))
+    created_at = db.Column(db.DateTime)
+    mode_int = db.Column(db.SmallInteger)
+
+
+# NOTES ON WHAT TO PRESERVE FROM API CALLS:
+# get user (/users/{user}/{mode}):
+#   id, avatar_url, country_code, username, monthly_playcounts, replays_watched_counts, badges
+# get user top scores (/users/{user}/scores/best?mode={mode}&limit=100):
+#   id, user_id, accuracy, created_at, pp, rank, beatmap{version}, beatmapset{artist, title, covers{list}}, maybe more here for aggregations
+# get user beatmap scores (/beatmaps/{beatmap}/scores/users/{user}/all):
+#   position, score{id, best_id, user_id, accuracy, mods, score, max_combo, statistics.count_300, statistics.count_100, statistics.count_50, passed, pp, rank, created_at, mode}
---
+++ app/services/request.py
@@ -0,0 +1,29 @@
+import os, json
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+base_url = "https://osu.ppy.sh/api/v2"
+token_url = "https://osu.ppy.sh/oauth/token"
+auth_base_url = "https://osu.ppy.sh/oauth/authorize"
+
+
+client = BackendApplicationClient(client_id=client_id, scope=["public"])
+session = OAuth2Session(client=client)
+token = session.fetch_token(
+    token_url=token_url, client_id=client_id, client_secret=client_secret
+)
+
+
+def send_request(url):
+    return session.request("GET", f"{base_url}{url}")
+
+
+r = send_request("/users/12408961/osu")
+print(r.content)
+
+# temp = json.dumps(r.json(), indent=4)
+# with open("test_3.json", "w") as outfile:
+# outfile.write(temp)
