cf7f43c8c60ec8036ea118723b2206ef0d75779b
JasonM610 <46823715+JasonM610@users.noreply.github.com>
2023-09-19 23:39:19 -0400

    update forms to fit db

--- app/home/forms.py
+++ app/home/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+from app.utils.enums import Mode
 
 
 class UserForm(FlaskForm):
-    user = StringField("User ID:", validators=[DataRequired()])
+    user = StringField("User ID: ", validators=[DataRequired()])
     mode = SelectField(
         "Gamemode: ",
         choices=[
-            ("osu", "osu!"),
+            ("osu", "osu"),
             ("taiko", "Taiko"),
             ("fruits", "CTB"),
             ("mania", "Mania"),
