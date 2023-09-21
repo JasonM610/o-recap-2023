ba861adccf125de3069d7af4d634b38785e1bbed
JasonM610 <46823715+JasonM610@users.noreply.github.com>
2023-09-20 21:29:31 -0400

    add insert into score, beatmap, top play dbs

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, redirect, render_template, url_for
+from app import db
 from app.home.forms import UserForm
 from app.utils.api import get_user_data, get_best_scores
 from app.utils.enums import Mode
@@ -14,17 +15,22 @@ def index():
     form = UserForm()
     if form.validate_on_submit():
         user_input = form.user.data
-        mode_input = Mode(form.mode.data)
+        mode_input = form.mode.data
 
         # collect user data from osu! api. store user_id for score endpoint
         user = get_user_data(user_input, mode_input)
-        user.upsert()
+        # user.upsert()
 
         user_id = user.user_id
 
         # if user isn't in database
         top_plays, scores, beatmaps = get_best_scores(user_id, mode_input)
+        db.session.merge(user)
+        [db.session.add(beatmap) for beatmap in beatmaps]
+        [db.session.add(score) for score in scores]
+        [db.session.add(top_play) for top_play in top_plays]
 
+        db.session.commit()
         # insert score beatmap & top play into db, merge user data if name was changed
 
         # return redirect(url_for("user", user=user))
