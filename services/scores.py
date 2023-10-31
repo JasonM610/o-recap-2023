60623168cfd20726cbbe427651eb43ffbeb11055
JasonM610 <46823715+JasonM610@users.noreply.github.com>
2023-10-31 15:38:59 -0400

    finish build_scores_df

--- services/scores.py
+++ services/scores.py
@@ -1,7 +1,7 @@
 import boto3, os, time
 import polars as pl
 from typing import Dict, Any
-from app.utils.osu import get_beatmap, get_beatmap_stats, get_beatmap_scores
+from app.utils.osu import get_beatmap, get_beatmap_attribs, get_beatmap_scores
 from services.beatmaps import collect_beatmap_ids
 
 dynamo = boto3.resource(
@@ -26,36 +26,42 @@ def build_scores_df(user_id: int, beatmaps_played: int) -> pl.DataFrame:
     scores = []
 
     for beatmap_id in collect_beatmap_ids(user_id, beatmaps_played):
-        beatmap_data = fetch_beatmap_data(beatmap_id) or get_beatmap(beatmap_id)
+        start = time.time()
+        beatmap_data = get_beatmap(beatmap_id)
+        if beatmap_data in ["graveyard", "wip", "pending"]:
+            continue
 
-        for score in get_beatmap_scores(user_id, beatmap_id).get("scores", []):
-            score_dict = score.to_dict()
-
-            mods = set(score_dict["mods"])
+        for score in get_beatmap_scores(user_id, beatmap_id):
+            mods = set(score.mods)
             if mods & {"EZ", "HR", "HT", "DT", "FL"}:
                 rate = 1.50 if "DT" in mods else 0.75 if "HT" in mods else 1.00
-                beatmap_data["bpm"] = int(beatmap_data["bpm"] * rate)
-                beatmap_data["hit_length"] = int(
-                    beatmap_data["hit_length"] * (1 / rate)
+                cs_scale = 1.30 if "HR" in mods else 0.50 if "EZ" in mods else 1.00
+
+                beatmap_attribs = get_beatmap_attribs(beatmap_id, {"mods": score.mods})
+                beatmap_data.update(
+                    {
+                        "difficulty_rating": beatmap_attribs["star_rating"],
+                        "hit_length": int(beatmap_data["hit_length"] * (1 / rate)),
+                        "bpm": int(beatmap_data["bpm"] * rate),
+                        "ar": beatmap_attribs["approach_rate"],
+                        "accuracy": beatmap_attribs["overall_difficulty"],
+                        "cs": max(10, beatmap_data["cs"] * cs_scale),
+                    }
                 )
 
-                # update ar, od, sr
+            score_dict = {
+                **score.to_dict(),
+                "star_rating": beatmap_data["difficulty_rating"],
+                "length": beatmap_data["hit_length"],
+                "bpm": beatmap_data["bpm"],
+                "ar": beatmap_data["ar"],
+                "od": beatmap_data["accuracy"],
+                "cs": beatmap_data["cs"],
+                "set_owner": beatmap_data["user_id"],
+            }
 
-            score_dict.update(
-                {
-                    "star_rating": beatmap_data["difficulty_rating"],
-                    "length": beatmap_data["hit_length"],
-                    "bpm": beatmap_data["bpm"],
-                    "ar": beatmap_data["ar"],
-                    "od": beatmap_data["accuracy"],
-                    "cs": beatmap_data["cs"],
-                    "set_owner": beatmap_data["user_id"],
-                }
-            )
+            score_dict["mods"] = ",".join(score_dict["mods"])
             scores.append(score_dict)
-
-        # temporary to handle API rate limit
-        time.sleep(0.1)
 
     return pl.DataFrame(scores)
 
