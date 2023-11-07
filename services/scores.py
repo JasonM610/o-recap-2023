3a25fb5d024b88ec27346500b632fb5cf37cd3a6
JasonM610 <46823715+JasonM610@users.noreply.github.com>
2023-11-07 00:25:47 -0500

    refactor build_scores_df for readability + fix errors in output

--- services/scores.py
+++ services/scores.py
@@ -1,6 +1,7 @@
 import boto3, os, time
 import polars as pl
-from typing import Dict, Any
+from typing import Dict, Any, List
+from app.models import Score
 from app.utils.osu import get_beatmap, get_beatmap_attribs, get_beatmap_scores
 from services.beatmaps import collect_beatmap_ids
 
@@ -23,45 +24,55 @@ def build_scores_df(user_id: int, beatmaps_played: int) -> pl.DataFrame:
     Returns:
         pl.DataFrame: A DataFrame containing every submitted score from a user
     """
+
+    def process_score() -> Dict[str, Any]:
+        mods = set(score.mods)
+        if mods & {"EZ", "HR", "HT", "DT", "NC", "FL"}:
+            rate = 1.5 if "DT" in mods or "NC" in mods else 0.75 if "HT" in mods else 1
+            cs_scale = 1.3 if "HR" in mods else 0.5 if "EZ" in mods else 1
+
+            beatmap_attribs = get_beatmap_attribs(beatmap_id, {"mods": score.mods})
+            beatmap_data.update(
+                {
+                    "difficulty_rating": round(beatmap_attribs["star_rating"], 2),
+                    "hit_length": int(beatmap_data["hit_length"] * (1 / rate)),
+                    "bpm": int(beatmap_data["bpm"] * rate),
+                    "ar": round(beatmap_attribs["approach_rate"], 2),
+                    "accuracy": round(beatmap_attribs["overall_difficulty"], 2),
+                    "cs": min(10, beatmap_data["cs"] * cs_scale),
+                }
+            )
+
+        score_dict = {
+            **score.to_dict(),
+            "mods": ",".join(score.mods),
+            "pp": score.pp if score.pp is not None else 0,
+            "star_rating": beatmap_data["difficulty_rating"],
+            "length": beatmap_data["hit_length"],
+            "bpm": beatmap_data["bpm"],
+            "ar": beatmap_data["ar"],
+            "od": beatmap_data["accuracy"],
+            "cs": beatmap_data["cs"],
+            "set_owner": beatmap_data["user_id"],
+        }
+
+        return score_dict
+
     scores = []
 
     for beatmap_id in collect_beatmap_ids(user_id, beatmaps_played):
         start = time.time()
         beatmap_data = get_beatmap(beatmap_id)
+
         if beatmap_data in ["graveyard", "wip", "pending"]:
             continue
 
         for score in get_beatmap_scores(user_id, beatmap_id):
-            mods = set(score.mods)
-            if mods & {"EZ", "HR", "HT", "DT", "FL"}:
-                rate = 1.50 if "DT" in mods else 0.75 if "HT" in mods else 1.00
-                cs_scale = 1.30 if "HR" in mods else 0.50 if "EZ" in mods else 1.00
+            score.append(process_score())
 
-                beatmap_attribs = get_beatmap_attribs(beatmap_id, {"mods": score.mods})
-                beatmap_data.update(
-                    {
-                        "difficulty_rating": beatmap_attribs["star_rating"],
-                        "hit_length": int(beatmap_data["hit_length"] * (1 / rate)),
-                        "bpm": int(beatmap_data["bpm"] * rate),
-                        "ar": beatmap_attribs["approach_rate"],
-                        "accuracy": beatmap_attribs["overall_difficulty"],
-                        "cs": max(10, beatmap_data["cs"] * cs_scale),
-                    }
-                )
-
-            score_dict = {
-                **score.to_dict(),
-                "star_rating": beatmap_data["difficulty_rating"],
-                "length": beatmap_data["hit_length"],
-                "bpm": beatmap_data["bpm"],
-                "ar": beatmap_data["ar"],
-                "od": beatmap_data["accuracy"],
-                "cs": beatmap_data["cs"],
-                "set_owner": beatmap_data["user_id"],
-            }
-
-            score_dict["mods"] = ",".join(score_dict["mods"])
-            scores.append(score_dict)
+        end = time.time()
+        if (end - start) < 0.1:
+            time.sleep(0.1 - (end - start))
 
     return pl.DataFrame(scores)
 
