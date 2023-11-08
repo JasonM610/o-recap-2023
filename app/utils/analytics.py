acf2b9090dde506532ae2aaaf7c1a9d41a1b6bd9
JasonM610 <46823715+JasonM610@users.noreply.github.com>
2023-11-07 22:20:48 -0500

    add 2023 pp and most played mods

--- app/utils/analytics.py
+++ app/utils/analytics.py
@@ -1,7 +1,8 @@
 import boto3, os
+import polars as pl
 from typing import Any, Dict, List
-from app.models import User, Score, BestScore
-from app.utils.osu import get_best_scores
+from app.models import User, BestScore
+from app.utils.osu import get_user, get_best_scores
 
 # sqs = boto3.client("sqs", region_name=os.environ.get("REGION"))
 # s3 = boto3.client("s3")
@@ -47,5 +48,45 @@ def build_initial_data(user: User, best_scores: List[BestScore]) -> Dict[str, An
     return initial_data
 
 
-def insert_score_analytics(user_id: int, scores: List[Score]) -> None:
-    pass
+def insert_score_analytics(user_id: int, scores: pl.DataFrame) -> None:
+    def get_2023_pp() -> int:
+        best_scores = scores.group_by("beatmap_id").agg(pl.col("pp").max())
+        score_ranking = best_scores["pp"].rank(method="ordinal", descending=True)
+
+        year_pp = (best_scores["pp"] * (0.95 ** (score_ranking - 1))).sum()
+
+        return round(year_pp, 3)
+
+    def get_highest_sr_pass() -> Dict[str, Any]:
+        passes = scores.filter(~scores["mods"].str.contains("NF"))
+
+        if passes.is_empty():
+            return {}
+
+        best_pass = passes[passes["star_rating"].arg_max()]
+
+        return {
+            "beatmap_id": best_pass["beatmap_id"][0],
+            "mods": best_pass["mods"][0],
+            "star_rating": round(best_pass["star_rating"][0], 2),
+        }
+
+    def get_habits() -> int:
+        map_counts = scores["set_owner"].value_counts(sort=True).head(3)
+        mod_counts = scores["mods"].value_counts(sort=True).head(3)
+
+        return {
+            "average_bpm": int(scores["bpm"].mean()),
+            "average_len": int(scores["length"].mean()),
+            "favorite_ar": scores["ar"].mode()[0],
+            "favorite_cs": scores["cs"].mode()[0],
+            "most_played_mapper": get_user(scores["set_owner"].mode()[0]).username,
+            "most_played_mods": mod_counts.to_dict(as_series=False),
+        }
+
+    analytics = {
+        "2023_pp": get_2023_pp(),
+        "highest_sr_pass": get_highest_sr_pass(),
+        "habits": get_habits(),
+    }
+    print(analytics)
