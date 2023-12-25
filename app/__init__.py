63f82a2d395071b0677550f95dc2c4beacd2aa2a
JasonM610 <46823715+JasonM610@users.noreply.github.com>
2023-12-24 23:55:22 -0500

    move vars to config file

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 def create_app() -> Flask:
     app = Flask(__name__, template_folder="templates")
     app.config.from_object("config.Config")
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
 
     with app.app_context():
         from .home import routes
--- config.py
+++ config.py
@@ -4,10 +4,16 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, ".env"))
 
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.environ.get("REGION")
+AWS_QUEUE_URL = os.environ.get("QUEUE_URL")
+
+AWS_PROFILE_TABLE = "ProfileData"
+AWS_BEATMAP_TABLE = "BeatmapData"
+
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     FLASK_APP = os.environ.get("FLASK_APP")
     FLASK_ENV = os.environ.get("FLASK_ENV")
-
-    QUEUE_URL = os.environ.get("QUEUE_URL")
